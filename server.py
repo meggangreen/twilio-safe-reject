81dbbed2aec4f9527484e19e5441d7cb47d5a30a
Meggan M Green <github@meggangreen.com>
2018-01-17 12:49:08 -0800

    getting basic files together

---
+++ server.py
@@ -0,0 +1,50 @@
+import requests
+import json
+
+from flask import Flask, render_template, redirect, request
+from flask import jsonify, flash, session
+from flask_debugtoolbar import DebugToolbarExtension
+from jinja2 import StrictUndefined
+
+
+app = Flask(__name__)
+
+# Required to use Flask sessions and the debug toolbar
+app.secret_key = "twilio safe reject"
+
+# Normally, if you use an undefined variable in Jinja2, it fails silently.
+# This is horrible. Make it raise an error instead.
+app.jinja_env.undefined = StrictUndefined
+
+
+@app.route('/')
+def index():
+    """ Index. """
+
+    return render_template("index.html")
+
+
+#########
+# Helper Functions
+#########
+
+if __name__ == "__main__":
+    # import sys
+
+    # app.debug has to be True at the point we invoke the DebugToolbarExtension
+    app.debug = True
+
+    # Ensure templates, etc. are not cached in debug mode
+    app.jinja_env.auto_reload = app.debug
+
+    # connect to test db if testing
+    # if 'test' in sys.argv[-1]:
+    #     connect_to_db(app, 'postgres:///twilioTEST')
+    # else:
+    #     connect_to_db(app, 'postgres:///twilioLIVE')
+
+    # Use the DebugToolbar
+    DebugToolbarExtension(app)
+
+    # Set server to localhost:5000
+    app.run(port=5000, host='0.0.0.0')
--- starterfile.py
+++ starterfile.py
@@ -2,15 +2,15 @@ import os
 from twilio.rest import Client
 
 # Your Account SID from twilio.com/console
-account_sid = os.environ['TWILIO_SID']
+account_sid = os.environ['TWILIO_TEST_SID']  # os.environ['TWILIO_SID']
 # Your Auth Token from twilio.com/console
-auth_token  = os.environ['TWILIO_TOK']
+auth_token  = os.environ['TWILIO_TEST_TOK']  # os.environ['TWILIO_TOK']
 
 client = Client(account_sid, auth_token)
 
 message = client.messages.create(
-    to=os.environ['TWILIO_MOB'],
-    from_=os.environ['TWILIO_BOT'],
+    to=os.environ['TWILIO_TEST_MOB'],  # os.environ['TWILIO_MOB'],
+    from_=os.environ['TWILIO_TEST_BOT'],  # os.environ['TWILIO_BOT'],
     body="Hello from Python!")
 
 print(message.sid)
