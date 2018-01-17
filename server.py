449fba0e96ad5a35526081d75ed295d1ab411539
Meggan M Green <github@meggangreen.com>
2018-01-17 23:04:01 +0000

    working on response logic

--- server.py
+++ server.py
@@ -1,35 +1,68 @@
-import requests
-import json
+""" Twilio SafeReject -- Safely reject their advances!
 
-from flask import Flask, render_template, redirect, request
-from flask import jsonify, flash, session
+    SafeReject lets registered users exchange fake contact information with
+    persistent would-be suiters, aka Harassers.
+
+    The user sends an SMS to the Twilio number with their credentials, the
+    harasser's phone number (and name, if available), and the short place name
+    where the exchange took place (eg: 'the bar' or 'BART'). SafeReject updates
+    its database with the information and then texts the harasser from a 'fake
+    phone number' while impersonating the user. This allows the user time to
+    safely get away from the harasser before the genius act of treachery is
+    discovered.
+
+    NEW USERS can sign up online ONLY at this time. We need to be able to
+    verify real users, and also to have a list of people who can't use the
+    service because they have been flagged as a harasser. Online signup
+    uses two-step authentication to verify the device.
+
+    REGISTERED USERS can get a new fake phone number or initiate a number
+    exchange between a fake phone number and a harasser. We verify users
+    by the 'From' and their PIN.
+
+    HARASSERS have been previously texted by a fake phone number via a
+    registered user -- eg the user has tried to avoid exchanging contact
+    info, but the harasser just doesn't get it and was then passed into the
+    system by the registered user. The harasser is now trying to converse
+    with the registered user.
+
+    UNKNOWNS are probably would be harassers trying to get the numbers so
+    they can "catch" registered users. And also spambots. Jerks.
+
+"""
+
+from flask import Flask, redirect, request, session
 from flask_debugtoolbar import DebugToolbarExtension
-from jinja2 import StrictUndefined
 
 from twilio.rest import Client
-from twilio.twiml.messaging_response import MessagingResponse
+from twilio.twiml.messaging_response import MessagingResponse, Message, Body
 
 
 app = Flask(__name__)
+app.config.from_object(__name__)
 
 # Required to use Flask sessions and the debug toolbar
 app.secret_key = "twilio safe reject"
 
-# Normally, if you use an undefined variable in Jinja2, it fails silently.
-# This is horrible. Make it raise an error instead.
-app.jinja_env.undefined = StrictUndefined
-
-
-@app.route('/')
-def index():
-    """ Index. """
-
-    return render_template("index.html")
-
 
 @app.route("/sms", methods=['GET', 'POST'])
 def sms_reply():
-    """Respond to incoming messages with a friendly SMS."""
+    """ Respond to texts from new users, registered users, and harassers. """
+
+    msg_from = request.values.get('From')
+    msg_body = request.values.get('Body', None)
+
+    # I'm pretty sure there is an error for empty messages, but I want them
+    # to fail silently so as not to alert harassers.
+    if not msg_body:
+        return
+
+    # Check if from number is in database, as user or harasser
+
+
+
+
+
 
     # Start our response
     resp = MessagingResponse()
